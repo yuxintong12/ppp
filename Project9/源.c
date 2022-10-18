d1d42f84884e42519c6f0f329944edfe0ed32a8b
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-10-18 15:14:04 +0800

    666

---
+++ Project9/源.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char input = 0;
+	char put;
+	while ((input = getchar()) != EOF)
+	{
+		put = input - 32;
+		putchar(put);
+		putchar('\n');
+		int define = 0;
+	}
+	return 0;
+
+}
---
+++ 排序/源.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+void bubble(int arr[], int sz)
+{
+	int i, j;
+	for (i = 0; i < sz - 1; i++)
+	{
+		for (j = 0; j < sz - 1 - i;j ++)
+		{
+			if (arr[j] < arr[j + 1])
+			{
+				int temp = 0;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+int main()
+{
+	int i = 0;
+	int arr[3] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	int sz;
+	sz = sizeof(arr) / sizeof(arr[0]);
+	bubble(arr,sz);
+	for (i = 0; i < sz ; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
