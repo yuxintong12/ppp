88307a594993c67a1ab7a58ec8c4acdae255813b
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-10-28 15:21:50 +0800

    新版倒序

--- Project13/源55.c
+++ Project13/源55.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include<assert.h>
 void ga(int x, int* a)
@@ -16,36 +17,12 @@ int main()
     int arr[4];
     int i = 0;
     int j = 0;
-    scanf("%d", &i); #define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-        void ga(int x, int* a)
-    {
-        static int i = 0;
-        *a++ = x % 10;
-        i++;
-        if (x > 9)
-        {
-            ga(x / 10, a);
-        }
-    }
-    int main()
-    {
-        int arr[4];
-        int i = 0;
-        int j = 0;
-        scanf("%d", &i);
-        ga(i, arr);
-        for (j = 0; j < 4; j++)
-        {
-            printf("%d", arr[j]);
-        }
-        return 0;
-    }
+    scanf("%d", &i);
     ga(i, arr);
     assert(arr != NULL);
     for (j = 0; j < 4; j++)
     {
-         printf("%d", arr[i]);
+        printf("%d", arr[j]);
     }
     return 0;
 }
