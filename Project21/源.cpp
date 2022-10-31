77068364fae8dd1191ed5bceee8d41f335d07c80
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-10-31 14:06:27 +0800

    66

---
+++ Project21/源.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+int main()
+{
+	int sum = 0;
+	int a = 0;
+	int b = 0;
+	cin >> a >> b;
+	sum = a + b;
+	cout << "ºÍÊÇ" << sum << endl;
+	system("pause");
+	return 0;
+}
