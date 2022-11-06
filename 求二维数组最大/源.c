e1b3000edb5f447ef3b7d38ca29adccb3521a436
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-11-06 22:02:54 +0800

    66

--- 求二维数组最大/源.c
+++ 求二维数组最大/源.c
@@ -1,38 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-void de(int arr[], int dz)
+void de(int arr[3][4])
 {
 	int i = 0;
-	int j;
-	int a = 0;
-	for (i = 1; i < dz; i++)
-	{
-		int te = arr[i];
-		int b = i;
-		for (j = i - 1; j >= 0; j--)
-		{
-			if (te < arr[j])
-			{
-				arr[b] = arr[j];
-					b--;
-			}
-			else
-			{
-				break;
-			}
-		}
-		arr[b] = te;
-	}
+	int j = 0;
+	
 }
 int main()
 {
-	int arr[10] = { 2,3,4,5,6,7,8,9,10,1 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	de(arr, sz);
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	return 0;
+	int arr[3][4] = { {2,3,6,5},{6,5,4,9},{9,6,8,7} };
+	de(arr);
 }
