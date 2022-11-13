b0084f6b94599c0b158c0862a10dc21724dd0e55
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-11-13 23:19:15 +0800

    44

---
+++ 实验报告2/源.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void print()
+{
+	printf("*******************\n");
+	printf("  Very Good!       \n");
+	printf("*******************\n");
+
+}
+int main()
+{
+	print();
+	return 0;
+}
