a4f05c5f4104853e2f72872b90f727ba00edaba0
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-11-21 15:49:55 +0800

    66

--- Project39/test.c
+++ Project39/test.c
@@ -1,37 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include <string.h>
-void bubble(int arr[], int sz)
-{
-	int i, j;
-	for (i = 0; i < sz - 1; i++)
-	{
-		for (j = 0; j < sz - 1 - i; j++)
-		{
-			if (arr[j] < arr[j + 1])
-			{
-				int temp = 0;
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-	}
-}
+#include "stdio.h"
 int main()
 {
-	int i = 0;
-	int arr[3] = { 0 };
-	for (i = 0; i < 3; i++)
+	int input;
+	scanf("%d", &input);
+	input /= 10;//ÒòÎªinput / 10 == 10-0
+	switch (input)
 	{
-		scanf("%d", &arr[i]);
-	}
-	int sz;
-	sz = sizeof(arr) / sizeof(arr[0]);
-	bubble(arr, sz);
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
+	case 10:
+		putchar('A');
+		break;
+	case 9:
+		putchar('A');
+		break;
+	case 8:
+		putchar('B');
+		break;
+	case 7:('D');
+		break;
+	default:
+		putchar('E');
+		break;
 	}
 	return 0;
 }
