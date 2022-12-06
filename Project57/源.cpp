52fa9b2eeb505829ae1b30e8d8cb7a17b540a50d
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-12-06 23:59:18 +0800

    三子棋

--- Project57/源.cpp
+++ Project57/源.cpp
@@ -1,12 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<easyx.h>
 #include<windows.h>
-#include<stdio.h>
 int main()
 {
 	initgraph(300, 600);
 	circle(3, 2, 60);
-	getchar();
+	system("pause");
 	closegraph();
 	return 0;
 }
---
+++ vscode/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 0;
+    cin >>a;
+    cout<<"77"<<a;
+    return 0;
+
+}
