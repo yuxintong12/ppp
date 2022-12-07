0a1f3793ee4097a71749bd62e97694e45c99417a
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-12-08 00:29:33 +0800

    6663

---
+++ Project58/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+int my_strcmp(const char* ch, const char* ch1)
+{
+	assert(ch && ch1);
+	while (*ch && *ch1&& *ch == *ch1)
+	{
+		ch++;
+		ch1++;
+	}
+	return *ch - *ch1;
+}
+int main()
+{
+	char ch1[20] = "kkkkk";
+	char ch[20] = "00myll";
+	int x = strcmp(ch, ch1);
+	int c = my_strcmp(ch, ch1);
+	return 0;
+}
