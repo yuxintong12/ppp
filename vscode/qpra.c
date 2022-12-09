158cb1d033b7ec20c41bc2deeea41bc2ea6281bf
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-12-09 15:44:53 +0800

    "666"

---
+++ vscode/qpra.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include<stdio.h>
+int cmp(const void* p,const void * p1)
+{
+    return (int)*(char *)p  - (int)*(char *)p1;
+}
+int main()
+{
+    char ch[10] = {"987524136"};
+    int sz = sizeof(ch) /sizeof(ch[0]);
+    qsort(ch,sz,sizeof(ch[0]),cmp);
+    printf("%s",ch + 1);
+    return 0;
+}
