693af424468f1102c7542bd25a17154eea651a62
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-12-09 22:13:01 +0800

    636

--- Project59/t.c
+++ Project59/t.c
@@ -7,9 +7,22 @@ int cmp(const void* p, const void* p1)
 }
 int main()
 {
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    int x = 0;
+    while (scanf("%d", &x) != EOF)
+    {
+        getchar();
+    }
+   
+  
+
     char ch[10] = { "987524136" };
     int sz = sizeof(ch) / sizeof(ch[0]);
-    qsort(ch, sz, sizeof(ch[0]), cmp);
+    qsort(ch, sz-1, sizeof(ch[0]), cmp);
     printf("%s", ch);
+    fclose(stdin);
+    fclose(stdout);
     return 0;
+    
 }
