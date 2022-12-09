280389217a3679d32974f7759153b8f22d6e3c13
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-12-09 22:00:21 +0800

    66

--- vscode/qpra.c
+++ vscode/qpra.c
@@ -1,14 +1,31 @@
-#include <stdlib.h>
-#include<stdio.h>
-int cmp(const void* p,const void * p1)
+#include <stdio.h>
+
+int main() 
 {
-    return (int)*(char *)p  - (int)*(char *)p1;
-}
-int main()
-{
-    char ch[10] = {"987524136"};
-    int sz = sizeof(ch) /sizeof(ch[0]);
-    qsort(ch,sz,sizeof(ch[0]),cmp);
-    printf("%s",ch + 1);
+    char c = 0;
+    scanf("%c",&c);
+    int line = 5;
+    int i = 0;
+    int j = 0;
+    for(i = 0;i < line ;i ++)
+    {
+        for(j = i;j < line-1;j ++)
+        {
+            printf(" ");
+        }
+        for(j = 0;j < i + 1; j++)
+        {
+            if(j > 0)
+            {
+                printf(" ");
+            }
+            printf("%c",c);
+        }
+         for(j = i;j < line-1;j ++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
     return 0;
 }
