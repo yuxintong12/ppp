deaa9a81fde0990897538976c7852b2148955666
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-12-09 22:53:26 +0800

    重定向的使用

--- Project59/t.c
+++ Project59/t.c
@@ -1,28 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include <stdlib.h>
-#include<stdio.h>
-int cmp(const void* p, const void* p1)
-{
-    return (int)*(char*)p - (int)*(char*)p1;
-}
+
+#include <stdio.h>
+
 int main()
 {
+    int i = 12;
+    int x = 0;
+
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
-    int x = 0;
-    while (scanf("%d", &x) != EOF)
-    {
-        getchar();
-    }
-   
-  
 
-    char ch[10] = { "987524136" };
-    int sz = sizeof(ch) / sizeof(ch[0]);
-    qsort(ch, sz-1, sizeof(ch[0]), cmp);
-    printf("%s", ch);
+    for (i = 12; i > 0; i--)
+    {
+
+        
+        scanf("%d", &x);
+        getchar();
+        printf("%c", x);
+
+
+    }
     fclose(stdin);
     fclose(stdout);
     return 0;
-    
 }
+    
--- vscode/qpra.c
+++ vscode/qpra.c
@@ -2,30 +2,14 @@
 
 int main() 
 {
+    int x = 0;
     char c = 0;
-    scanf("%c",&c);
-    int line = 5;
-    int i = 0;
-    int j = 0;
-    for(i = 0;i < line ;i ++)
+    while( c != '\n')
     {
-        for(j = i;j < line-1;j ++)
-        {
-            printf(" ");
-        }
-        for(j = 0;j < i + 1; j++)
-        {
-            if(j > 0)
-            {
-                printf(" ");
-            }
-            printf("%c",c);
-        }
-         for(j = i;j < line-1;j ++)
-        {
-            printf(" ");
-        }
-        printf("\n");
+        scanf("%d",&x);
+        c = getchar();
+        printf("%c",x);
+       
     }
     return 0;
 }
