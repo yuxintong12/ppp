3bbeee3c155033e0e5d09c626e227b0aba34a204
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-12-12 13:08:44 +0800

    new

--- 五子棋博弈（不含图形库）/fun.h
+++ 五子棋博弈（不含图形库）/fun.h
@@ -1,1 +1,4 @@
 #pragma once
+#define HA 10
+#define LI 10
+#define zi 5
--- 五子棋博弈（不含图形库）/test.c
+++ 五子棋博弈（不含图形库）/test.c
@@ -1,1 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include "fun.h"
+void menu()
+{
+	printf("******************\n");
+	printf("   1.play 2.exit  \n");
+	printf("******************\n");
+}
+void play()
+{
+	char bord[HA] = { 0 };
+	char show[LI] = { 0 };
+
+}
+int main()
+{
+	char judge = 0;
+	do
+	{
+		puts("来吧展示:>");
+		menu();
+		judge = getchar();
+		getchar();
+		switch (judge)
+		{
+		case '1':
+			play();
+			break;
+		case '2':
+			break;
+		default:
+			printf("怎么回事\n");
+			break;
+		}
+	} while (judge != '2');
+	return 0;
+}
