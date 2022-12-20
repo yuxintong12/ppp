4c1fa0e2acb1a3c44dc7f6186626485bdc378d52
yuxintong12 <110955477+yuxintong12@users.noreply.github.com>
2022-12-20 23:46:25 +0800

    new

---
+++ Project60/源.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+void* my_memcpy(void* dest, const void* scr, size_t n)
+{
+    void* ret = dest;
+    while ((*(char*)dest = *(char*)scr) && n--)
+    {
+        (char*)dest = (char*)dest + 1;
+        (char*)scr = (char*)scr + 1;
+    }
+    return ret;
+}
+void* my_memmove(void* dest, const void* scr, size_t n)
+{
+    if (dest > scr)
+    {
+        void* ret = dest;
+        while ((*(char*)dest = *(char*)scr) && n--)
+        {
+            ;
+        }
+        return ret;
+    }
+    else
+    {
+        void* tem = dest;
+        while (n--)
+        {
+            *((char*)dest + n) = *((char*)scr + n);
+        }
+        return tem;
+
+    }
+}
+int main()
+{
+    char ch[10] = "kjkjkak";
+    char ch1[10] = "llkll";
+    int a = memcmp(ch, ch1, 5);
+    my_memcpy(ch, ch + 1, 3);
+    printf("%s\n", ch);
+    memcpy(ch, ch + 1, 3);
+    printf("%s\n", ch);
+    memmove(ch, ch + 1, 3);
+    printf("%s\n", ch);
+    my_memmove(ch, ch + 1, 3);
+    printf("%s\n", ch);
+    return 0;
+}
--- vscode/.vscode/my001.c
+++ vscode/.vscode/my001.c
@@ -1,9 +1,48 @@
 #include<stdio.h>
 #include<string.h>
+void * my_memcpy(void* dest,const void *scr,size_t n)
+{
+    void*ret = dest;
+    while((*(char*)dest++ = *(char*)scr++) && n--)
+    {
+       int  x = 1;
+    }
+    return ret;
+}
+void * my_memmove(void *dest,const void *scr,size_t n)
+{
+    if(dest > scr)
+    {
+        void*ret = dest;
+        while((*(char*)dest++ = *(char*)scr++) && n--)
+        {
+            ;
+        }
+        return ret;
+    }
+    else
+    {
+        void*tem = dest;
+        while(n--)
+        {
+            *((char*)dest + n) = *((char*)scr + n);
+        }
+        return tem;
+
+    }
+}
 int main()
 {
     char ch[10] = "kjkjkak";
     char ch1[10] ="llkll";
     int a = memcmp(ch,ch1,5);
+    my_memcpy(ch,ch + 1,3);
+    printf("%s\n",ch);
+    memcpy(ch,ch + 1,3);
+    printf("%s\n",ch);
+    memmove(ch,ch + 1,3);
+    printf("%s\n",ch);
+    my_memmove(ch,ch + 1,3);
+    printf("%s\n",ch);
     return 0;
 }
